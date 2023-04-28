9d45c701fe1b038f83ac2daf695c977f7019f6f7
Tak3sh1 <professionaltakeshi@gmail.com>
2023-04-29 01:09:41 +0900

    Project's Initial Commit

---
+++ www/JS/motor.js
@@ -0,0 +1,20 @@
+function carregar() {
+
+localStorage.setItem("nome", document.getElementById("coletortexto").value);
+localStorage.setItem("senha", document.getElementById("coletortexto2").value);
+
+alert("Inserido");
+}
+
+    function carregar2() {
+
+    localStorage.setItem("Lnome", document.getElementById("loginNome").value);
+    localStorage.setItem("Lsenha", document.getElementById("loginSenha").value);
+alert("Inserido");
+}
+function exibir(){
+    if (localStorage.getItem("Lnome") == localStorage.getItem("nome") && localStorage.getItem("Lsenha") == localStorage.getItem("senha"))
+    {
+                document.getElementById("ata").textContent = "Parabéns tu recebeu MB";
+    }
+}
